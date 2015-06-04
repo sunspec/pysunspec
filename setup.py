4514ea28b37a56bf23da85d367b06f46c2d7f40b
bobfox <bob.fox@loggerware.com>
2015-06-04 10:27:00 -0700

    Updated to version 1.0.6.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from distutils.core import setup
 
 setup(name = 'pysunspec',
-      version = '1.0.5',
+      version = '1.0.6',
       description = 'Python SunSpec Tools',
       author = ['Bob Fox'],
       author_email = ['bob.fox@loggerware.com'],
